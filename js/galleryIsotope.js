0cefcab2c11a8ad65d2d16f58346f1e24f2e13b1
anthonyfajardo <anthonyfajardo@gmail.com>
2017-02-17 21:56:56 -0500

    Update galleryIsotope.js

--- js/galleryIsotope.js
+++ js/galleryIsotope.js
@@ -1,20 +1,33 @@
 $(function(){
+// 	var $grid = $('.grid').isotope({
+// 		itemSelector: '.grid-item', 
+// 		stagger: 30
+// 	});
+
+// 	$('.filter-button-group').on('click', '.button', function(){
+// 		var filterValue = $(this).attr('data-filter');
+// 		$grid.isotope({filter: filterValue});
+// 	});
+
+// 	$('.button-group').each(function(i, buttonGroup){
+// 		var $buttonGroup = $( buttonGroup );
+// 		$buttonGroup.on('click', 'button', function(){
+// 			$buttonGroup.find('.is-checked').removeClass('is-checked');
+// 			$(this).addClass('is-checked');
+// 		});
+// 	});
+
+		// init Isotope
 	var $grid = $('.grid').isotope({
-		itemSelector: '.grid-item', 
-		stagger: 30
+	  // options
+	  itemSelector: '.grid-item',
+	  stagger:30
+	});
+	// filter items on button click
+	$('.filter-button-group').on( 'click', '.button', function() {
+	  var filterValue = $(this).attr('data-filter');
+	  $grid.isotope({ filter: filterValue });
 	});
 
-	$('.filter-button-group').on('click', '.button', function(){
-		var filterValue = $(this).attr('data-filter');
-		$grid.isotope({filter: filterValue});
-	});
-
-	$('.button-group').each(function(i, buttonGroup){
-		var $buttonGroup = $( buttonGroup );
-		$buttonGroup.on('click', 'button', function(){
-			$buttonGroup.find('.is-checked').removeClass('is-checked');
-			$(this).addClass('is-checked');
-		});
-	});
-
+	
 });
